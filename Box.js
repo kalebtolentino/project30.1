9929330246f0380d35f3839750a50bb0f12be828
kalebtolentino <71675572+kalebtolentino@users.noreply.github.com>
2020-12-08 17:31:11 -0800

    Add files via upload
    .;

---
+++ Box.js
@@ -0,0 +1,39 @@
+class Block {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.1,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed < 3){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("green");
+        rect(0, 0, this.width, this.height);
+        pop();
+       }
+       else{ 
+         push();
+         World.remove(world, this.body);
+         console.log("red")
+         this.Visiblity = this.Visiblity - 5;
+         tint(255, this.Visiblity);
+         //image(this.body.position.x, this.body.position.y, 50, 50);
+         pop();
+        }
+      }
+      
+    }
+  ;
+  
